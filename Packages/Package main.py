6579038faf809bb601ec8e3b69b2097211eb0781
Yoga-satwika <Veernalayoga.satwika@diggibyte.in>
2025-10-28 22:04:41 +0530

    Add files via upload

---
+++ Packages/Package main.py
@@ -0,0 +1,7 @@
+from math package import add, subtract
+
+result1 = math package.add(5, 3)
+result2 = math package.subtract(10, 4)
+
+print("Addition:", result1)
+print("Subtraction:", result2)
---
+++ Packages/Packagemain.py
@@ -0,0 +1,7 @@
+from packagemain import add, subtract
+
+result1 = packagemain.add(5, 3)
+result2 = packagemain.subtract(10, 4)
+
+print("Addition:", result1)
+print("Subtraction:", result2)
